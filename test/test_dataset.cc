c6282c71b7aeb3b7d955df5240f3b22e5fd2ddf8
stonelin <jason840708@gmail.com>
2018-05-11 01:56:08 +0800

    test dataset with preview

--- test/test_dataset.cc
+++ test/test_dataset.cc
@@ -1,6 +1,7 @@
 #include <fmt/format.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
 #include "dataset.h"
-
 int main(int argc, const char* argv[]) {
   dataset data;
 
@@ -9,15 +10,35 @@ int main(int argc, const char* argv[]) {
     return -1;
   }
 
-  data.load(argv[1], 48, 40);
+  constexpr int h = 48;
+  constexpr int w = 48;
+  data.load(argv[1], h, w);
   data.get(0.1);
-  for (int i = 0; i < 10; ++i) {
-    cv::imshow("image", data.image[i]);
-    cv::imshow("lbp", data.image_lbp[i]);
-    auto v = data.label.at<int>(0, i);
-    std::cout << v << '\n';
-    cv::waitKey(0);
+  cv::Mat preview_pos(h * 2, 0, CV_8UC3);
+  cv::Mat preview_neg(h * 2, 0, CV_8UC3);
+  int pos = 0;
+  int neg = 0;
+  int i = 0;
+  while ((pos < 10) || (neg < 10)) {
+    // cv::imshow("image", data.image[i]);
+    // cv::imshow("lbp", data.image_lbp[i]);
+    cv::Mat m;
+    cv::cvtColor(data.image_lbp[i], m, CV_GRAY2BGR);
+    cv::vconcat(data.image[i], m, m);
+    auto l = data.label.at<int>(0, i);
+    if ((pos < 10) && (l == 1)) {
+      cv::hconcat(preview_pos, m, preview_pos);
+      ++pos;
+    } else if (neg < 10 && (l == -1)) {
+      cv::hconcat(preview_neg, m, preview_neg);
+      ++neg;
+    }
+    ++i;
   }
+  cv::Mat preview;
+  cv::vconcat(preview_pos, preview_neg, preview);
+  cv::imshow("preview", preview);
+  cv::waitKey(0);
 
   return 0;
 }
